0919b1c93dd7916e59ff1d45eee6a2d58b99ffd5
Jasper <148862656+seed745@users.noreply.github.com>
2024-03-11 16:11:50 +0000

    added server initialisation

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
+
+app = dash.Dash(__name__)
+server = app.server
+app.layout = html.Div([html.H1("Hello world!"), html.H3("Live long and prosper")]) 
+if __name__ == '__main__':
+    app.run_server(debug=True)
